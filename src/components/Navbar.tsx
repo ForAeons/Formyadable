993cb2c04826dc22b8911ccf356111e127c1c8b1
W. Xiaoyun <w.xy020203@gmail.com>
2022-12-15 21:37:00 +0800

    Adding components files

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { useState } from "react";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">some text</div>
+    </nav>
+  );
+}
